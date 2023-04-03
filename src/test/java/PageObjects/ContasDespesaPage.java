2aceddb34971a44d3e04e554faf5fffb9bd42f79
rafaelalyns <127249548+rafaelalyns@users.noreply.github.com>
2023-04-03 10:37:03 -0300

    Add files via upload

---
+++ src/test/java/PageObjects/ContasDespesaPage.java
@@ -0,0 +1,23 @@
+package PageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ContasDespesaPage {
+    private WebDriver driver;
+    public ContasDespesaPage(WebDriver driver) {
+        this.driver = driver;
+    }
+    public WebElement getLogoText() {
+        
+        
+        return driver.findElement(By.className("navbar-brand"));
+    }
+    public WebElement getNomeConta() {
+        return driver.findElement(By.id("nome"));
+    }
+    public WebElement getBotaoSalvar() {
+        return driver.findElement(By.xpath("//div[@class = 'btn-group']/div[@class = 'btn btn-primary]"));
+    }
+}
